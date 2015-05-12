46c15bb3e67017774ac93a7d48a70508f84864cc
Baabuseek <baabuseek@gmail.com>
2015-05-12 12:50:16 +0200

    Utworzenie pliku z konfiguracją bazy danych.

---
+++ configuration.php
@@ -0,0 +1,8 @@
+<?php
+
+$DBASE['host'] = "localhost"; // nazwa hosta
+$DBASE['name'] = "l8-02"; // nazwa bazy danych
+$DBASE['username'] = "root"; // uzytkownik
+$DBASE['password'] = "root"; // hasło
+
+?>
