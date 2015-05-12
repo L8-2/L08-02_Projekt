dd3eaab2282030b31c940a4a5269237ac46c55e1
mdrzymala <drzymek93@gmail.com>
2015-05-12 22:30:59 +0200

    Uzupełnienie konstruktora i destruktora

--- model/model.php
+++ model/model.php
@@ -1,14 +1,40 @@
 <?php
 abstract class Model extends Controller
 {
+	public $mysql;
+	
 	public function __construct()
 	{
+		try
+		 {
+            require_once('configuration.php');
+			$connection = mysql_connect($DBASE['host'], $DBASE['username'], $DBASE['password']);
 
+			if($connection)
+			{
+				$this->mysql = $connection;
+				
+				mysql_select_db($DBASE['name']);
+
+				if(mysql_error())
+					throw new Exception('Nie mo¿na odnaleŸæ bazy '.$DBASE['name']);
+			}
+			else
+				throw new Exception('Nie mo¿na po³¹czyæ siê z baz¹ danych w: '.$path);
+				
+			include_once("view/header.phtml");
+                  }
+
+        catch(Exception $e)
+		{
+			$this->throw_error($e);
+		}
 	}
 	
 	public function __destruct()
 	{
-
+		include_once(dirname($_SERVER['SCRIPT_FILENAME'])."/view/footer.phtml");
+		mysql_close($this->mysql);
 	}
 
 }
