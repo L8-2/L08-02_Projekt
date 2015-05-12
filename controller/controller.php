1e144dcb8a0d01dff9d749d415ea34497be82685
krzysiek926 <krzysiek20083@wp.pl>
2015-05-12 19:41:42 +0200

    Konstruktor

--- controller/controller.php
+++ controller/controller.php
@@ -3,7 +3,24 @@ class Controller
 {
 	public function __construct()
     {
-
+	try
+		{
+			$model = (isset($_GET['con'])?$_GET['con']:'main');
+			$path = 'model/'.$model.'.php';
+			$model .= "_Model"; 
+			
+			if(is_file($path))
+			{
+				include_once($path);
+				$this->model = new $model;
+			}
+			else
+				throw new Exception('Nie można otworzyć pliku modelu: '.$path);
+		}
+        catch(Exception $e)
+		{
+			$this->throw_error($e);
+        }
     }
 
 }
