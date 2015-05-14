8cda7a6141fe6003ac28622adc764812850b5bad
krzysiek926 <krzysiek20083@wp.pl>
2015-05-14 19:34:08 +0200

    Rejestracja poprawka

--- model/rejestracja.php
+++ model/rejestracja.php
@@ -20,21 +20,22 @@ class rejestracja_Model extends Model
 	{
 		if(isset($_POST['rejestruj'])) 
 		{
-			$result = $this->sql_query("INSERT INTO Konto INSERT INTO `konto`(`Login`, `Haslo`, `Imie`, `Nazwisko`, `Email`, `Nr_Telefonu`)VALUES ('".addslashes($_POST['login'])."', '".addslashes($_POST['haslo'])."', 
-			'".addslashes($_POST['imie'])."','".addslashes($_POST['nazwisko'])."','".addslashes($_POST['email'])."',
-			'".addslashes($_POST['nr_telefonu'])."'"); 
-			
-		
+			$result = $this->sql_query("SELECT * FROM `konto` WHERE `login`='".addslashes($_POST['login'])."' LIMIT 1"); 
+	
 			if($_POST['login'] == "" || $_POST['haslo'] == "" || $_POST['imie'] == "" || $_POST['nazwisko'] == "" ||
 				$_POST['email'] == "" || $_POST['nr_telefonu'] == "")
-				{
 					$this->redirect("index.php?con=rejestracja", "error", "Nie wprowadzono danych."); 
-				  }
-			
+			else if($result) 
+				$this->redirect("index.php?con=rejestracja", "error", "Takie konto już istnieje."); 
 			 else 
 			 {
-				$_SESSION['rejestracja'] = true; 
-				$this->redirect("index.php", "success", "Utworzyłeś pomyślnie konto!"); 
+				
+				 
+
+				mysql_query("INSERT INTO `konto`(`Login`, `Haslo`, `Imie`, `Nazwisko`, `Email`, `Nr_Telefonu`)VALUES ('".addslashes($_POST['login'])."', md5('".addslashes($_POST['haslo'])."'), 
+			'".addslashes($_POST['imie'])."','".addslashes($_POST['nazwisko'])."','".addslashes($_POST['email'])."',
+			'".addslashes($_POST['nr_telefonu'])."')")or die(mysql_error()) ;
+				$this->redirect("index.php?con=login", "success", "Utworzyłeś pomyślnie konto! Możesz się teraz zalogować."); 
 			}
 			
 		}
