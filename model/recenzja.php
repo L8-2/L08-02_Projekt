f2eb2acf9d61d1e3b831d53f9b940e677fc627da
mdrzymala <drzymek93@gmail.com>
2015-05-17 19:18:40 +0200

    edycja i usuwanie recenzji

--- model/recenzja.php
+++ model/recenzja.php
@@ -9,7 +9,11 @@ class recenzja_Model extends Model
 		
 		switch ($this->getAction()) {
 			case 'dodaj':
-			    return $this->addRecenzja();
+			    return $this->dodaj();
+			case 'edytuj':
+			    return $this->edytuj();
+			case 'usun':
+			    return $this->usun();
 		    default:
 		    	$this->index();
 		}
@@ -31,7 +35,7 @@ class recenzja_Model extends Model
 		include __DIR__ . "/../view/recenzja_index.phtml";
 	}
 	
-	public function addRecenzja()
+	public function dodaj()
 	{
 	    if (!isset($_GET['id_artykul'])) {
 	        $this->redirect("index.php?con=recenzja", "error", "Musisz wybrać artykuł.");
@@ -50,11 +54,17 @@ class recenzja_Model extends Model
 	    
 	    $q = mysql_query(sprintf('SELECT 1 FROM artykul WHERE ID_Artykul = %d', $idArtykul));
 	
+	    $recenzjaForm = array(
+	        'tresc' => '',
+	        'ocena' => 0,
+	        'publikuj' => false
+	    );
 	    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	        $recenzjaForm = $_POST;
 	        $tresc = isset($_POST['tresc']) ? $_POST['tresc'] : null;
 	        $ocena = isset($_POST['ocena']) ? (int) $_POST['ocena'] : null;
 	        $publikuj = isset($_POST['publikuj']) ? (bool) $_POST['publikuj'] : null;
-	        if (!$tresc || !$ocena || $publikuj === null) {
+	        if (!$tresc || $ocena === null || $publikuj === null) {
 	            $this->setMessage('error', 'Wypełnij wszystkie pola');
 	        } elseif (!in_array($ocena, range(-5, 5))) {
 	            $this->setMessage('error', 'Błędna ocena');
@@ -72,7 +82,7 @@ class recenzja_Model extends Model
             		mysql_real_escape_string($tresc),
             		$recenzent['ID_Recenzent'],
     	            $idArtykul,
-            		$ocena,
+            		$idOcena,
             		$publikuj
         		));
     	        if (!$q) {
@@ -83,7 +93,88 @@ class recenzja_Model extends Model
 	        }
 	    }
 	
-	    include __DIR__ . '/../view/recenzja_dodaj.phtml';
+	    include __DIR__ . '/../view/recenzja_ceform.phtml';
+	}
+	
+	public function edytuj()
+	{
+	    if (!isset($_GET['id'])) {
+	        $this->redirect("index.php?con=recenzja", "error", "Musisz wybrać artykuł.");
+	    }
+	     
+	    $recenzent = $this->findRecenzentByKonto($_SESSION['id']);
+	    $id = (int) $_GET['id'];
+	     
+	    $recenzja = $this->findOne(sprintf(
+	        'SELECT r.*, o.Ocena AS Ocena FROM recenzja r INNER JOIN ocena o ON r.ID_Ocena = o.ID_Ocena WHERE ID_Recenzja = %d AND ID_Recenzent = %d',
+	        $id,
+	        $recenzent['ID_Recenzent']
+	    ));
+	    if (!$recenzja) {
+	        $this->redirect("index.php?con=recenzja", "error", "Wybrana recenzja nie istnieje.");
+	    }
+	    
+	    $recenzjaForm = array(
+	        'tresc' => $recenzja['Tresc'],
+	        'ocena' => $recenzja['Ocena'],
+	        'publikuj' => (bool) $recenzja['Opublikowana']
+	    );
+	    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	        $recenzjaForm = $_POST; 
+	        $tresc = isset($_POST['tresc']) ? $_POST['tresc'] : null;
+	        $ocena = isset($_POST['ocena']) ? (int) $_POST['ocena'] : null;
+	        $publikuj = isset($_POST['publikuj']) ? (bool) $_POST['publikuj'] : null;
+	        if (!$tresc || $ocena === null|| $publikuj === null) {
+	            $this->setMessage('error', 'Wypełnij wszystkie pola');
+	        } elseif (!in_array($ocena, range(-5, 5))) {
+	            $this->setMessage('error', 'Błędna ocena');
+	        } else {
+	            $q1 = mysql_query(sprintf("
+        	        UPDATE ocena
+        	        SET Ocena = %d
+	                WHERE ID_Ocena = %d
+                ", $ocena, $recenzja['ID_Ocena']));
+        	    $q2 = mysql_query(sprintf("
+        	        UPDATE recenzja
+        	        SET Tresc = '%s', Opublikowana = %d
+        	        WHERE ID_Recenzja = %d
+                ", mysql_real_escape_string($tresc), $publikuj, $id));
+	            if (!$q1 || !$q2) {
+	                throw new RuntimeException('Nie można zaaktulizować recenzji');
+	            }
+	             
+	            $this->redirect("index.php?con=recenzja", "success", "Zaaktualizowano recenzję");
+	        }
+	    }
+	     
+	    include __DIR__ . '/../view/recenzja_ceform.phtml';
+	}
+	
+	public function usun()
+	{
+	    if (!isset($_GET['id'])) {
+	        $this->redirect("index.php?con=recenzja", "error", "Musisz wybrać artykuł.");
+	    }
+	    
+	    $recenzent = $this->findRecenzentByKonto($_SESSION['id']);
+	    $id = (int) $_GET['id'];
+	    
+	    $recenzja = $this->findOne(sprintf(
+	        'SELECT * FROM recenzja WHERE ID_Recenzja = %d AND ID_Recenzent = %d',
+	        $id,
+	        $recenzent['ID_Recenzent']
+        ));
+	    if (!$recenzja) {
+	        $this->redirect("index.php?con=recenzja", "error", "Wybrana recenzja nie istnieje.");
+	    }
+	    
+	    $q1 = mysql_query(sprintf('DELETE FROM recenzja WHERE ID_Recenzja = %d', $id));
+	    $q2 = mysql_query(sprintf('DELETE FROM ocena WHERE ID_Ocena = %d', $recenzja['ID_Ocena']));
+	    if (!$q1 || !$q2) {
+	        throw new RuntimeException("Wystąpił błąd podczas usuwania recenzji.");
+	    } else {
+	        $this->redirect("index.php?con=recenzja", "success", "Usunięto recenzję.");
+	    }
 	}
 	
 	private function isAllowedToAdd($idArtykul, $idRecenzent)
