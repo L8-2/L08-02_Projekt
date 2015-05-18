ecd7f91ebbff88d545f742aa91d3b228c709a46a
krzysiek926 <krzysiek20083@wp.pl>
2015-05-18 13:31:05 +0200

    Aktualizacja dziennika Zdarzen

--- model/dziennikZdarzen.php
+++ model/dziennikZdarzen.php
@@ -11,51 +11,50 @@ class dziennikZdarzen_Model extends Model
 				$this->dziennikZdarzen();
 			
 	 include_once("view/dziennikZdarzen.phtml");
-				
-		
+
 	}
 	
 
  
 	public function dziennikZdarzen() 
 	{
-		if(isset($_POST['Dodaj'])) 
+		
+						$buttonSzukajKonf=0;
+						$buttonSzukajRec=0;
+		if(isset($_POST['szukaj'])) 
 		{
 			
-			//if ( $this->isLogged())
-			//{
+			if ( $this->isAdmin())
+			{
 				
 				
-			
-				//if (($_POST['data_od'] ==""  || $_POST['data_do']=="") &($_POST['limit_miejsc']==""  ))
-				//{
-					//$this->redirect("index.php?con=dziennikZdarzen", "error", "Wypelnij pola");
-				//}
+	
+					
+		/*	$konferencje=mysql_query("SELECT * FROM konferencja WHERE 
+			Data >= '".addslashes($_POST['data_od'])."' AND 
+			Data <='".addslashes($_POST['data_do'])."' OR Limit_Miejsc >='".addslashes($_POST['limit_miejsc'])."'")or die(mysql_error()) ;*/
 				
-				//else{
-			$wynik=mysql_query("SELECT * FROM `konferencja` WHERE Data >= '".addslashes($_POST['data_od'])."' AND 
-				Data <='".addslashes($_POST['data_do'])."' OR Limit_Miejsc >='".addslashes($_POST['limit_miejsc'])."'")or die(mysql_error()) ;
+				$konferencje=mysql_query("SELECT konto.Login, konferencja.Nazwa,konferencja.Data, konferencja.Miejsce, konferencja.Limit_Miejsc, konferencja.Program, konferencja.Termin_Zgloszen,konferencja.Koszt
+				FROM konferencja LEFT JOIN organizator ON konferencja.ID_Konferencja=organizator.ID_Konferencja 
+				LEFT JOIN konto ON konto.ID_Konto=organizator.ID_Konto");
 				
-					//$this->redirect("index.php?con=dziennikZdarzen", "success", "Konferencja została dodana do bazy");
+					$recenzje=mysql_query("SELECT konto.Login, recenzja.Tresc, ocena.Ocena FROM recenzja 
+							LEFT JOIN recenzent ON recenzja.ID_Recenzent=recenzent.ID_Recenzent
+							LEFT JOIN konto ON recenzent.ID_Konto=konto.ID_Konto 
+							LEFT JOIN ocena ON ocena.ID_Ocena=recenzja.ID_Ocena")or die(mysql_error()) ;
+							
+
+
 				include __DIR__ . "/../view/wyniki_dziennika.phtml";
-			//	}
+
+			}
 			
-			// echo "<td bgcolor=\"ffff99\">" . $result[3][3] . "</td>";
-			 
-		
- 
-		//$this->redirect("index.php?con=dziennikZdarzen", "success", "Konferencja została dodana do bazy");
-			//$wynik= $this->sql_query("SELECT * FROM konto"); 
-			//$a=$wynik[0][0];
-		
-			
-			//}
+			else 
+				$this->redirect("index.php?con=dziennikZdarzen", "error", "Nie masz uprawnień");
 				
 		
 		
 		}
-	
-
 		
 	}
 }
