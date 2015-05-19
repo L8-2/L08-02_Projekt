b7a9d8ab96307df0b88c1855d45e8131874ae384
GaWi007 <pawel_gawi@wp.pl>
2015-05-19 18:50:39 +0200

    Modyfikacja strony głównej

--- model/main.php
+++ model/main.php
@@ -6,7 +6,21 @@ class main_Model extends Model
 	public function __construct()  
 	{
 		parent::__construct();
+		$this->main();
 		include_once("view/main.phtml");
 	}
+	
+	public function main()
+	{
+		
+		$result = $this->sql_query("SELECT k.ID_Konferencja, k.Nazwa, kon.Imie, kon.Nazwisko, k.Miejsce, k.Data, k.Koszt, k.Data_Utworzenia
+									FROM konferencja k 
+									join organizator o on o.ID_Organizator=k.ID_Organizator
+									join konto kon on kon.ID_Konto=o.ID_Konto ORDER BY k.Data_Utworzenia DESC");
+
+		include __DIR__ . "/../view/main.phtml";
+	}
+	
+	
 }
 ?>
