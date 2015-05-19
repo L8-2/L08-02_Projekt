3020a626dca275afa131f2cbeea3dad857817110
wojtek109 <wojtek109@gmail.com>
2015-05-19 20:55:17 +0200

    Zarządzanie recenzentami przez organizatora koferencji

--- model/zarz_recenzentami.php
+++ model/zarz_recenzentami.php
@@ -9,92 +9,34 @@ class zarz_recenzentami_Model extends Model
 	public function __construct()
 	{
 		parent::__construct();
-			switch($this->getAction())
-			{
-			case 'dodaj':
-			{
-				//$this->zarz_recenzentami = $this->load();
-				$this->zarz_recenzentami= $this->dodaj();
-				include_once("view/zarz_recenzentami.phtml"); 
-			}
-			case 'usun' :
-			{
-				$this->zarz_recenzentami= $this->usun();
-				include_once("view/zarz_recenzentami.phtml"); 
-			}
-		default:
-		{
+			
 				$this->zarz_recenzentami = $this->load();
 				include_once("view/zarz_recenzentami.phtml");
-				break;
+				
 		}
-	}
-	}
-	public function dodaj()
-	{
-			$selected_val2=  $_POST['decision1'];
-			echo $selected_val2;
-			if ($selected_val2 !="Wybierz recenzenta do dodania")
-			{
-				if (empty($_GET['konf']))
-					$selected_val=0;
-				else
-					$selected_val= $_GET['konf']; 
-				mysql_query("INSERT INTO `recenzent` (ID_Konto , ID_Konferencja ) VALUES
-				('".addslashes($selected_val2)."','".addslashes($selected_val)."')")
-				or die(mysql_error());
-				$this->redirect("index.php?con=zarz_recenzentami&konf=$selected_val&s1=1", "success", "Dodano recenzenta");
-			}
-	}
+	
+	
+	
 			
 
 	public function load()
 	{	
-		
-		
-			if(isset($_POST['submit']))
-				$this->redirect("index.php?con=zarz_recenzentami&konf=$s&s1=1", "error", "LAPL");
-		if(isset($_POST['submit2']))
+			if(isset($_POST['submit2']))
 			{
-			$selected_val= $_POST['decision'];
-			$s=$selected_val-1;
-			$this->redirect("index.php?con=zarz_recenzentami&konf=$s&s1=1", "", "");
+				$selected = $_POST['decision'];
+				$this->redirect("index.php?con=zarz_recenzentami_2&konf=$selected", "", "");
 			}
-		if (empty($_GET['konf']))
-			 $selected_val=0;
-		else
-			 $selected_val= $_GET['konf'];
-			 $wynik[] = $selected_val;
-		$selected_val2=0;
 		if(isset($_SESSION['id']))
 		{
 			$result = $this->sql_query("SELECT k.Nazwa ,k.ID_Konferencja FROM  konferencja k  ,organizator o 
 			WHERE  (k.ID_Organizator =  o.ID_Organizator)  AND  (o.ID_Konto = '".addslashes($_SESSION['id'])."')   ")
 			or die(mysql_error());
-		
 				$wynik[] = $result;
-				
-			
-			
-				
 		}
 		else
 			$this->redirect("index.php?con=login", "error", "Musisz się zalogować!");
-		
-		if (!(empty($_GET['s1'])))
-			{ 
-			$res = $this->sql_query("SELECT ko.Imie , ko.Nazwisko ,  ko.ID_Konto FROM konto ko  
-			, recenzent r  WHERE (r.ID_Konferencja = '".addslashes($wynik[1][$selected_val]['ID_Konferencja'])."') 
-			AND ( r.ID_Konto = ko.ID_Konto )") or die (mysql_error());
-			$wynik[] = $res;
-			}
-			
 			return $wynik;
 	}
 	
-	public function usun()
-	{
-		
-	}
 }
 ?>
---
+++ model/zarz_recenzentami_2.php
@@ -0,0 +1,99 @@
+<?php
+include_once("model/model.php");
+class zarz_recenzentami_2_Model extends Model
+{
+	public $wynik = array();
+	public function __construct()
+	{
+		parent::__construct();
+			
+			
+		
+				$this->zarz_recenzentami_2 = $this->load();
+				include_once("view/zarz_recenzentami_2.phtml");
+			
+		
+	
+	}
+	
+			
+
+	public function load()
+	{	
+		
+		$k= $_GET['konf'];
+		if(isset($_POST['submit2']))
+		{
+			$selected = $_POST['decision'];
+			mysql_query("DELETE FROM recenzent WHERE (ID_Recenzent = '".addslashes($selected)."')")
+			or die(mysql_error());
+			$this->redirect("index.php?con=zarz_recenzentami_2&konf=$k", "success", "Usunięto recenzenta");
+			
+		}
+		if(isset($_POST['submit4']))
+		{
+			$result = $this->sql_query("SELECT * FROM `konto` WHERE `login`='".addslashes($_POST['login'])."' LIMIT 1"); 
+	
+			if($_POST['login'] == "" || $_POST['haslo'] == "" || $_POST['imie'] == "" || $_POST['nazwisko'] == "" ||
+				$_POST['email'] == "" || $_POST['nr_telefonu'] == "")
+					$this->redirect("index.php?con=zarz_recenzentami_2&konf=$k", "error", "Nie wprowadzono danych."); 
+			else if($result) 
+				$this->redirect("index.php?con=zarz_recenzentami_2&konf=$k", "error", "Takie konto już istnieje."); 
+			 else 
+			 {
+				
+				 
+
+				 mysql_query("INSERT INTO `konto` (`Login`, `Haslo`, `Imie`, `Nazwisko`, `Email`, `Nr_Telefonu`)
+			  VALUES ('".addslashes($_POST['login'])."', md5('".addslashes($_POST['haslo'])."'), 
+			'".addslashes($_POST['imie'])."','".addslashes($_POST['nazwisko'])."','".addslashes($_POST['email'])."',
+			'".addslashes($_POST['nr_telefonu'])."')")or die(mysql_error()) ;
+				
+				$result = $this->sql_query("SELECT * FROM konto");
+				
+				mysql_query("INSERT INTO recenzent (ID_Konto , ID_Konferencja ) VALUES
+			('".addslashes(mysql_num_rows($result))."','".addslashes($k)."') ")
+			or die(mysql_error());
+			
+			$this->redirect("index.php?con=zarz_recenzentami_2&konf=$k", "success", "Pomyślnie dodano recenzenta");
+			}
+			
+		}
+		if(isset($_POST['submit3']))
+		{
+			$selected = $_POST['decision2'];
+			mysql_query("INSERT INTO recenzent (ID_Konto , ID_Konferencja ) VALUES
+			('".addslashes($selected)."','".addslashes($k)."')")
+			or die(mysql_error());
+			$this->redirect("index.php?con=zarz_recenzentami_2&konf=$k", "success", "Pomyślnie dodano recenzenta");
+			
+		}
+		
+		if(isset($_SESSION['id']))
+		{
+			$wynik[] =  $this->sql_query("SELECT k.Nazwa  FROM konferencja k WHERE (k.ID_Konferencja = '".addslashes($k)."')")
+			or die(mysql_error());
+				
+				
+		}
+		else
+			$this->redirect("index.php?con=login", "error", "Musisz się zalogować!");
+		
+			$res = $this->sql_query("SELECT * FROM konto WHERE (ID_Konto !=  '".addslashes($_SESSION['id'])."')") or die(mysql_error());
+			
+			$wynik[] = $res; 
+			$res = $this->sql_query("SELECT r.ID_Recenzent ,ko.Imie , ko.Nazwisko ,  ko.ID_Konto FROM konto ko , recenzent r  WHERE (r.ID_Konferencja = '".addslashes($k)."') 
+			AND ( r.ID_Konto = ko.ID_Konto )") ;
+						
+			$wynik[] = $res;
+			
+			
+			return $wynik;
+	
+	}
+	public function usun()
+	{
+			$this->redirect("index.php?con=login", "error", "Musisz się zalogować!");	
+	}
+}
+?>
