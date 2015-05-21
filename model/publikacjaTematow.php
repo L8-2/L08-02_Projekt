2a11efa6721f43df314612cdf2fe9a768f326ce6
krzysiek926 <krzysiek20083@wp.pl>
2015-05-21 09:08:04 +0200

    Dołączenie Publikacji Tematów do Administratora

--- model/publikacjaTematow.php
+++ model/publikacjaTematow.php
@@ -17,16 +17,28 @@ class publikacjaTematow_Model extends Model
 	
 	public function index()
 	{
+		
+			if ( $this->isAdmin()){
 		$tematy = $this->sql_query('
 		    SELECT * 
-		    FROM tematy WHERE zaakceptowany=0
+		    FROM tematy WHERE zaakceptowany=0 OR  zaakceptowany IS NULL
 	    ');
 		
 		include __DIR__ . "/../view/publikacjaTematow.phtml";
+		
+		
+			}
+			else
+				$this->redirect("index.php", "error", "Nie masz uprawnień");
+
 	}
 	
 	public function aktualizuj()
 	{
+		
+		if ( $this->isAdmin())
+			{
+		
 	    if ($_SERVER['REQUEST_METHOD'] != 'POST') {
 	        $this->redirect("index.php?con=publikacjaTematow", "error", "Błędne żądanie.");
 	    } elseif (!isset($_POST['id']) || !is_array($_POST['id'])) {
@@ -41,5 +53,11 @@ class publikacjaTematow_Model extends Model
 		$q = mysql_query(sprintf('UPDATE `tematy` SET `Zaakceptowany`= 1 WHERE ID_Tematy IN (%s)', implode(',', $ids)));
 	    
 	    $this->redirect("index.php?con=publikacjaTematow", "success", "Wybrane tematy zostały zaakceptowane");
+		
+			}
+			
+	
+				else
+					$this->redirect("index.php", "error", "Nie masz uprawnień");
 	}
 }
