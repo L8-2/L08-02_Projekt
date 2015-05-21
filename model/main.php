667862f2092150be8b3cb5d38bf38b71f931c6f9
Łukasz <misio1771@gmail.com>
2015-05-21 14:21:32 +0200

    Poprawa strony głównej cd.

--- model/main.php
+++ model/main.php
@@ -3,19 +3,15 @@ include_once("model/model.php");
 
 class main_Model extends Model
 {
+
 	public function __construct()  
 	{
 		parent::__construct();
-		$this->main();
+		
+		$result = $this->sql_query("SELECT * FROM `konferencja` ORDER BY `Data_Utworzenia` DESC LIMIT 6");
+
 		include_once("view/main.phtml");
 	}
-	
-	public function main()
-	{
-		$result = $this->sql_query("SELECT * FROM `konferencja` ORDER BY `Data_Utworzenia` DESC");
-		return $result;
-	}
-	
-	
+
 }
 ?>
