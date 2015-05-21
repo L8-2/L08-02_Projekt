7c91054f49bca53f4e7f40870a9d78fc219aa16d
Konrad S <baabuseek@gmail.com>
2015-05-21 13:31:47 +0200

    Stworzenie wyglądu dla strony głównej

--- model/main.php
+++ model/main.php
@@ -12,13 +12,8 @@ class main_Model extends Model
 	
 	public function main()
 	{
-		
-		$result = $this->sql_query("SELECT k.ID_Konferencja, k.Nazwa, kon.Imie, kon.Nazwisko, k.Miejsce, k.Data, k.Koszt, k.Data_Utworzenia
-									FROM konferencja k 
-									join organizator o on o.ID_Organizator=k.ID_Organizator
-									join konto kon on kon.ID_Konto=o.ID_Konto ORDER BY k.Data_Utworzenia DESC");
-
-		include __DIR__ . "/../view/main.phtml";
+		$result = $this->sql_query("SELECT * FROM `konferencja` ORDER BY `Data_Utworzenia` DESC");
+		return $result;
 	}
 	
 	
