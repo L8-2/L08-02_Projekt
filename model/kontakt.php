2efb36dd35740b981bfdf431c759b440fb714330
krzysiek926 <krzysiek20083@wp.pl>
2015-05-21 13:51:44 +0200

    Kontakt

---
+++ model/kontakt.php
@@ -0,0 +1,21 @@
+<?php
+include_once("model/model.php"); 
+
+class kontakt_Model extends Model 
+{
+	public function __construct()  
+	{
+		parent::__construct(); 
+		
+				
+	 include_once("view/kontakt.phtml");
+
+	}
+	
+
+ 
+
+	
+	
+}
+?>
