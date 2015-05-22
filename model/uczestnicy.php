88a4a62c3a503e7c056362b938e970a613607bc0
Łukasz Sieradzki <misio1771@gmail.com>
2015-05-22 17:31:24 +0200

    Zarządzanie uczestnikami

--- model/uczestnicy.php
+++ model/uczestnicy.php
@@ -10,6 +10,10 @@ class uczestnicy_Model extends Model
 		switch ($this->getAction()) {
 			case 'usun':
 			    return $this->usun();
+			case 'accept':
+				return $this->accept();
+			case 'reject':
+				return $this->reject();
 		    default:
 		    	$this->index();
 		}
@@ -53,4 +57,57 @@ class uczestnicy_Model extends Model
 	    
 	    $this->redirect("index.php?con=uczestnicy", "success", "Usunięto wybranych uczestników");
 	}
+	
+	public function accept()
+	{
+	if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+	        $this->redirect("index.php?con=uczestnicy", "error", "Błędne żądanie.");
+	    } elseif (!isset($_POST['id']) || !is_array($_POST['id'])) {
+	        $this->redirect("index.php?con=uczestnicy", "error", "Musisz wybrać uczestników do zaakceptowania.");
+	    }
+		$ids = array();
+	    foreach ($_POST['id'] as $id) {
+	       $ids[] = (int) $id;        
+        }
+		
+	    $q = mysql_query(sprintf('SELECT 1 FROM uczestnik WHERE ID_Konto IN (%s)', implode(',', $ids)));
+	    if (mysql_num_rows($q) !== count($ids)) {
+	        $this->redirect("index.php?con=uczestnicy", "error", "Przesłano błędne dane.");
+	    }
+	    
+	    $q = mysql_query(sprintf('UPDATE uczestnik SET Zaakceptowany = 1 WHERE ID_Konto IN (%s)', implode(',', $ids)));
+	    if (!$q) {
+	        throw new RuntimeException('Wystąpił błąd przy akceptacji uczestników');
+	    }
+	    
+	    $this->redirect("index.php?con=uczestnicy", "success", "Zaakceptowano wybranych uczestników");
+	
+	}
+	
+		public function reject()
+	{
+	if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+	        $this->redirect("index.php?con=uczestnicy", "error", "Błędne żądanie.");
+	    } elseif (!isset($_POST['id']) || !is_array($_POST['id'])) {
+	        $this->redirect("index.php?con=uczestnicy", "error", "Wybierz uczestników, których chcesz odrzucić.");
+	    }
+		$ids = array();
+	    foreach ($_POST['id'] as $id) {
+	       $ids[] = (int) $id;        
+        }
+		
+	    $q = mysql_query(sprintf('SELECT 1 FROM uczestnik WHERE ID_Konto IN (%s)', implode(',', $ids)));
+	    if (mysql_num_rows($q) !== count($ids)) {
+	        $this->redirect("index.php?con=uczestnicy", "error", "Przesłano błędne dane.");
+	    }
+	    
+	    $q = mysql_query(sprintf('UPDATE uczestnik SET Zaakceptowany = 0 WHERE ID_Konto IN (%s)', implode(',', $ids)));
+	    if (!$q) {
+	        throw new RuntimeException('Wystąpił błąd podczas odrzucania uczestników');
+	    }
+	    
+	    $this->redirect("index.php?con=uczestnicy", "success", "Odrzucono wybranych uczestników");
+	
+	}
+	
 }
