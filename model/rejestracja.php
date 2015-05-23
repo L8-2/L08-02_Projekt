67db21a8c9afa8f20122e6d6a26e5b0c12e826d7
Konrad S <baabuseek@gmail.com>
2015-05-23 15:29:09 +0200

    Poprawa przy walidacji wprowadzonych danych

--- model/rejestracja.php
+++ model/rejestracja.php
@@ -16,8 +16,12 @@ class rejestracja_Model extends Model
 	{
 		if(isset($_POST['rejestruj']))
 		{
+			$result = $this->sql_query("SELECT * FROM `konto` WHERE `login`='".addslashes($_POST['login'])."' LIMIT 1"); 
+
 			if($_POST['login'] == "" || $_POST['haslo'] == "" || $_POST['imie'] == "" || $_POST['nazwisko'] == "" || $_POST['email'] == "" || $_POST['nr_telefonu'] == "")
 				$this->redirect("index.php?con=rejestracja", "error", "Nie wprowadzono danych."); 
+			else if($result) 
+				$this->redirect("index.php?con=rejestracja", "error", "Takie konto już istnieje."); 
 			else if(strlen($_POST['imie']) > 20 || !preg_match('@^[a-zA-Z]{3,20}$@', $_POST['imie']))
 				$this->redirect("index.php?con=rejestracja", "error", "Podane imię jest nieprawidłowe!"); 
 			else if(strlen($_POST['nazwisko']) > 40 || !preg_match('@^[a-zA-Z]{2,40}$@', $_POST['nazwisko']))
@@ -26,8 +30,6 @@ class rejestracja_Model extends Model
 				$this->redirect("index.php?con=rejestracja", "error", "Adres e-mail jest nieprawidłowy!");
 			else if(strlen($_POST['nr_telefonu']) > 20 || !preg_match('@^[0-9]{6,20}$@', $_POST['nr_telefonu']))
 				$this->redirect("index.php?con=rejestracja", "error", "Numer telefonu jest nieprawidłowy!");
-			else if($result) 
-				$this->redirect("index.php?con=rejestracja", "error", "Takie konto już istnieje."); 
 			else 
 			{
 				mysql_query("INSERT INTO `konto`(`Login`, `Haslo`, `Imie`, `Nazwisko`, `Email`, `Nr_Telefonu`)VALUES ('".addslashes($_POST['login'])."', md5('".addslashes($_POST['haslo'])."'), 
