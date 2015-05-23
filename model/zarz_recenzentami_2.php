39d6a094197d91f1f4b1f61835f195554a2edd10
Konrad S <baabuseek@gmail.com>
2015-05-23 15:32:13 +0200

    Poprawa wyglądu oraz walidacji danych

--- model/zarz_recenzentami_2.php
+++ model/zarz_recenzentami_2.php
@@ -39,20 +39,25 @@ class zarz_recenzentami_2_Model extends Model
 					$this->redirect("index.php?con=zarz_recenzentami_2&konf=$k", "error", "Nie wprowadzono danych."); 
 			else if($result) 
 				$this->redirect("index.php?con=zarz_recenzentami_2&konf=$k", "error", "Takie konto już istnieje."); 
-			 else 
-			 {
-				
-				 
-
+			else if(strlen($_POST['imie']) > 20 || !preg_match('@^[a-zA-Z]{3,20}$@', $_POST['imie']))
+				$this->redirect("index.php?con=rejestracja", "error", "Podane imię jest nieprawidłowe!"); 
+			else if(strlen($_POST['nazwisko']) > 40 || !preg_match('@^[a-zA-Z]{2,40}$@', $_POST['nazwisko']))
+				$this->redirect("index.php?con=rejestracja", "error", "Podane nazwisko jest nieprawidłowe!");
+			else if(strlen($_POST['email']) > 60 || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+				$this->redirect("index.php?con=rejestracja", "error", "Adres e-mail jest nieprawidłowy!");
+			else if(strlen($_POST['nr_telefonu']) > 20 || !preg_match('@^[0-9]{6,20}$@', $_POST['nr_telefonu']))
+				$this->redirect("index.php?con=rejestracja", "error", "Numer telefonu jest nieprawidłowy!");
+			else 
+			{
 				 mysql_query("INSERT INTO `konto` (`Login`, `Haslo`, `Imie`, `Nazwisko`, `Email`, `Nr_Telefonu`)
 			  VALUES ('".addslashes($_POST['login'])."', md5('".addslashes($_POST['haslo'])."'), 
 			'".addslashes($_POST['imie'])."','".addslashes($_POST['nazwisko'])."','".addslashes($_POST['email'])."',
 			'".addslashes($_POST['nr_telefonu'])."')")or die(mysql_error()) ;
 				
-				$result = $this->sql_query("SELECT * FROM konto");
+				$id = mysql_insert_id(); 
 				
 				mysql_query("INSERT INTO recenzent (ID_Konto , ID_Konferencja ) VALUES
-			('".addslashes(mysql_num_rows($result))."','".addslashes($k)."') ")
+			('".$id."','".addslashes($k)."') ")
 			or die(mysql_error());
 			
 			$this->redirect("index.php?con=zarz_recenzentami_2&konf=$k", "success", "Pomyślnie dodano recenzenta");
