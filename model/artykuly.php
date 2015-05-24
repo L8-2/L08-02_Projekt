d2e9ed85fb96f70ceff4586635d24898b6dc2d89
mdrzymala <drzymek93@gmail.com>
2015-05-24 14:45:03 +0200

    niewielka modernizacja artykułów

--- model/artykuly.php
+++ model/artykuly.php
@@ -10,25 +10,90 @@ class artykuly_Model extends Model
 		parent::__construct(); 
 		
 		
-		$this->artykuly();
-		
-		
+		switch ($this->getAction()) {
+		    case 'pokaz':
+		        $this->pokaz();
+		        break;
+		    default:
+		        $this->artykuly();
+		        break;
+		}
 	}
+	
+	public function pokaz()
+	{
+	    if (!isset($_GET['id'])) {
+	        $this->redirect("index.php?con=recenzja", "error", "Musisz wybrać artykuł.");
+	    }
+	    
+	    $idArt = (int) $_GET['id'];
+		$artykul = $this->findOne(sprintf('SELECT * FROM artykul WHERE ID_Artykul = %d', $idArt));
+		if (!$artykul) {
+		    $this->redirect("index.php?con=recenzja", "error", "Wybrany artykuł nie istnieje.");
+		}
+		
+		$type = 'user';
+		if ($this->isAdmin()) {
+		    $type = 'admin';
+		} elseif ($this->isOrganizator()) {
+		    $type = 'organizator';
+		} elseif ($this->isRecenzent()) {
+		    $type = 'recenzent';
+		} elseif ($artykul['ID_Konto'] == $this->getLoggedId()) {
+		    $type = 'autor';
+		}
+		
+		switch ($type) {
+		    case 'admin':
+		    case 'organizator':
+		    case 'autor':
+        		$recenzje = $this->findAll(sprintf(
+        		    'SELECT r.*, k.Imie AS Autor_Imie, k.Nazwisko AS Autor_Nazwisko 
+        		     FROM recenzja r 
+        		     INNER JOIN recenzent rc ON r.ID_Recenzent = rc.ID_Recenzent 
+        		     INNER JOIN konto k ON k.ID_Konto = rc.ID_Konto
+    		         INNER JOIN ocena o ON o.ID_Ocena = r.ID_Ocena
+        		     WHERE r.ID_Artykul = %d AND r.Opublikowana = 1',
+        		     $idArt
+        	    ));
+        		break;
+		    case 'recenzent':
+		        $recenzje = $this->findOne(sprintf(
+		            'SELECT r.*, k.Imie AS Autor_Imie, k.Nazwisko AS Autor_Nazwisko
+        		     FROM recenzja r
+		             INNER JOIN recenzent rc ON r.ID_Recenzent = rc.ID_Recenzent 
+        		     INNER JOIN konto k ON k.ID_Konto = rc.ID_Konto
+	                 INNER JOIN ocena o ON o.ID_Ocena = r.ID_Ocena
+        		     WHERE r.ID_Artykul = %d AND r.Opublikowana = 1 AND k.ID_Konto = %d',
+		            $idArt,
+		            $this->getLoggedId()
+		        ));
+		        if (!$recenzje) {
+		            $recenzje = array();
+		        } else {
+		            $recenzje = array($recenzje);    
+		        }
+		        
+		        break;
+		    case 'user':
+	        default:
+	            $recenzje = array();
+		}
+		
+		include __DIR__ . "/../view/artykuly_pokaz.phtml";
+	}
+	
 	public function artykuly() 
 	{		
 		if($this->isAdmin() || $this->isOrganizator() || $this->isRecenzent())
 		{
-
-	
 			$artykuly = $this -> sql_query("SELECT Tytul,ID_Artykul
 			FROM artykul 
 			WHERE Opublikowany!='0'
 			AND ID_Konferencja =".addslashes($_SESSION['id_konf'])."");
-			
 		}
 		else
 		{
-			
 			$artykuly = $this -> sql_query("SELECT Tytul,ID_Artykul
 			FROM artykul
 			WHERE Opublikowany!='0'
@@ -36,21 +101,24 @@ class artykuly_Model extends Model
 			AND ID_Konto = ".addslashes($_SESSION['id'])."");	
 			
 		}
-		if(isset($_POST['wroc']))
-		{
-			
-			include __DIR__ . "/../view/artykuly.phtml";
-			
-		}
-		else if(!isset($_POST['wroc']))
-		{
-			
-			include __DIR__ . "/../view/artykuly.phtml";
-			
-		}
 		
+		include __DIR__ . "/../view/artykuly.phtml";
 	}
 	
+	private function filterPokaz(array &$conds)
+	{
+	    if ($_SERVER['REQUEST_METHOD'] != 'POST' || !isset($_POST['grupuj'])) {
+	        return;
+	    }
+	    
+	    if (isset($_POST['ocena'])) {
+	        if (!in_array($_POST['ocena'], range(-5, 5))) {
+	            $this->setMessage('error', 'Błędna ocena, poprawne oceny to od -5 do 5');
+	        } else {
+	             $conds[] = sprintf('o.Ocena = %d', $_POST['ocena']);
+	        }
+	    }
+	}
 }
 	
 ?>
