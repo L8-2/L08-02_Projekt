8174f6a3ac94c693c9d19e08085fcd445a99fa86
Konrad S <baabuseek@gmail.com>
2015-05-26 02:19:51 +0200

    Poprawa stylu oraz ilości wyświetlanych konferencji

--- model/main.php
+++ model/main.php
@@ -8,7 +8,7 @@ class main_Model extends Model
 	{
 		parent::__construct();
 		
-		$result = $this->sql_query("SELECT * FROM `konferencja` ORDER BY `Data_Utworzenia` DESC LIMIT 6");
+		$result = $this->sql_query("SELECT * FROM `konferencja` ORDER BY `Data_Utworzenia` DESC LIMIT 8");
 
 		include_once("view/main.phtml");
 	}
