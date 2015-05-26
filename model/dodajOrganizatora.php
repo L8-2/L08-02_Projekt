2799b4f07453b5911b2c8806c259f5322c8febae
wojtek109 <wojtek109@gmail.com>
2015-05-26 12:38:31 +0200

    poprawa błędu

--- model/dodajOrganizatora.php
+++ model/dodajOrganizatora.php
@@ -20,7 +20,7 @@ class dodajOrganizatora_Model extends Model
 		if(isset($_SESSION['id']))
 		{
 		
-			$result = $this->sql_query("SELECT k.Nazwa ,k.ID_Konferencja FROM  konferencja k  RIGHT JOIN organizator o  ON k.ID_Organizator =  o.ID_Organizator
+			$result = $this->sql_query("SELECT k.Nazwa ,k.ID_Konferencja FROM  konferencja k  RIGHT JOIN organizator o  ON k.ID_Konferencja =  o.ID_Konferencja
 			WHERE  (o.ID_Konto = '".addslashes($_SESSION['id'])."')   ")
 			or die(mysql_error());
 			
